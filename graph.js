d084f302a4aa32d1c4daabbfbfba6f5a00c1c8dc
Vicente Mata <matavic25@hotmail.com>
2020-03-25 14:02:05 -0400

    Added d3 update pattern function

--- graph.js
+++ graph.js
@@ -15,3 +15,51 @@ const svg = d3.select('.canvas')
 
 const graph = svg.append('g')
     .attr('transform', `translate(${cent.x},${cent.y})`);
+
+const pie = d3.pie()
+    .sort(null)
+    .value(d => d.cost);
+
+const angles = pie([
+    {name:'rent', cost:500},
+    {name:'bills', cost:300},
+    {name:'gaming', cost:200}
+]);
+
+const arcPath = d3.arc()
+    .outerRadius(dims.radius)
+    .innerRadius(dims.radius / 2);
+// update function
+const update = (data) => {
+    const paths = graph.selectAll('path')
+        .data(pie(data));
+
+    paths.enter()
+        .append('path')
+        .attr('class', 'arc')
+        .attr('d', arcPath)
+        .attr('stroke', '#fff')
+        .attr('stroke-width', 3);
+}
+
+var data = [];
+db.collection('expenses').onSnapshot(res => {
+    res.docChanges().forEach(change => {
+        const doc = {...change.doc.data(), id: change.doc.id}
+        switch (change.type) {
+            case "added":
+                data.push(doc);
+                break;
+            case "removed":
+                data = data.filter(item => item.id !== doc.id);
+                break;
+            case "modified":
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index]= doc;
+                break;
+            default:
+                break;
+        }
+    });
+    update(data);
+});
